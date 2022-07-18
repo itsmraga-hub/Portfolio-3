9f01991eb8df7aa5d64c48a728ab284f1e712969
Raga <ragawilliam570@gmail.com>
2022-07-18 10:17:54 +0300

    Added app.js and edited it

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // eslint-disable-next-line linebreak-style
 const primaryNav = document.querySelector('.primary-navigation');
 const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+const navLinks = document.querySelectorAll('.nav-links li');
 
 mobileNavToggle.addEventListener('click', () => {
   const visible = primaryNav.getAttribute('data-visible');
@@ -13,4 +14,12 @@ mobileNavToggle.addEventListener('click', () => {
     primaryNav.setAttribute('data-visible', false);
     mobileNavToggle.setAttribute('aria-expanded', false);
   }
+
+  navLinks.forEach((link, i) => {
+    if (link.getElementsByClassName.animation) {
+      link.style.animation = '';
+    } else {
+      link.style.animation = `fadein 500ms ease forwards ${i / 7 + 0.3}s`;
+    }
+  });
 });
